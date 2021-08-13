308a3d829cc7eaa9eeb6a480186a16c1b616912e
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2021-08-13 15:23:56 +0100

    added a couple of pages

---
+++ src/Start.svelte
@@ -0,0 +1,20 @@
+<script>
+  import C from './constants'
+  import {
+    gameState,
+    name
+  } from './stores'
+</script>
+
+<h2>Ok, let's get this show on the road!</h2>
+<p>Tell me your name. No jokes, I promise.</p>
+
+<input bind:value={$name} />
+
+<button
+  on:click={() => gameState.update(g => g = C.gameStates.main)}
+  disabled={!$name}
+  type=submit
+>
+  Submit
+</button>
